ab0e36fe9cffeb389176fbf19ff624c1beef70aa
efiewoncharso51017008 <48231310+efiewoncharso51017008@users.noreply.github.com>
2019-09-12 12:25:27 +0800

    prak1
    12 September 2019

---
+++ percobaan3.java
@@ -0,0 +1,30 @@
+class percobaan3 {
+public static void  main(String [] args) {
+	
+System.out.println("operasi Aritmatika"+"pada tipe integer") ;
+int a = 2 + 1;
+int b = a - 1;
+int c = a * 1;
+int d = c / 1;
+int e = -a ;
+System.out.println("Nilai a: " + a);
+System.out.println("Nilai b: " + b);
+System.out.println("Nilai c: " + c);
+System.out.println("Nilai d: " + d);
+System.out.println("Nilai e: " + e);
+System.out.println ();
+	
+System.out.println("Operasi Aritmatika " + " pada tipe floating-point");	
+double fa = 2 + 1 ;
+double fb = fa - 1 ;
+double fc = fa * 1 ;
+double fd = fc / 1 ;
+double fe = -fa ;
+	
+System.out.println("Nilai fa: " + fa);
+System.out.println("Nilai fb: " + fb);
+System.out.println("Nilai fc: " + fc);
+System.out.println("Nilai fd: " + fd);
+System.out.println("Nilai fe: " + fe);
+}
+}
---
+++ percobaan6.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class percobaan6 {
+	public static void main(String[] args) {
+	Scanner input = new Scanner (System.in);
+	int nilai1, nilai2, hasil;
+	
+	System.out.print("masukkan integer pertama:");
+		nilai1 = input.nextInt();
+	
+	System.out.print("masukkan integer pertama:");
+		nilai2 = input.nextInt();
+		
+	hasil = nilai1 + nilai2;
+	System.out.print("Hasil" +nilai1+ " + " +nilai2+ " = " +hasil);
+	}
+		
+}
